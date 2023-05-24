ed3c3fcbb9c9daaf059d4e79b0594cf06b5e11e8
Timo Renk <renk.git@gmail.com>
2023-05-24 02:45:06 +0200

    refactor: change u64 to usize

--- user_shared/src/sys_call.rs
+++ user_shared/src/sys_call.rs
@@ -3,8 +3,8 @@ use core::arch::asm;
 use riscv_utils as riscv;
 use riscv_utils::SysCall;
 
-unsafe fn system_call(syscall: SysCall, param_0: u64, param_1: u64) -> u64 {
-    let number = syscall as u64;
+unsafe fn system_call(syscall: SysCall, param_0: usize, param_1: usize) -> usize {
+    let number = syscall as usize;
     riscv::write_function_reg!(
         number => "a7",
         param_0 => "a0",
@@ -18,7 +18,7 @@ unsafe fn system_call(syscall: SysCall, param_0: u64, param_1: u64) -> u64 {
 
 pub fn print_char(char: char) {
     unsafe {
-        system_call(SysCall::PrintChar, char as u64, 0);
+        system_call(SysCall::PrintChar, char as usize, 0);
     }
 }
 
@@ -29,7 +29,7 @@ pub fn get_char() -> Option<char> {
         if res == 0 {
             return None;
         }
-        return Some(res as u8 as char);
+        Some(res as u8 as char)
     }
 }
 
@@ -38,11 +38,7 @@ pub fn print(string: &str) {
         return;
     }
     unsafe {
-        system_call(
-            SysCall::PrintString,
-            string.as_ptr() as u64,
-            string.len() as u64,
-        );
+        system_call(SysCall::PrintString, string.as_ptr() as usize, string.len());
     }
 }
 //todo this should be changed to one system call..
@@ -50,7 +46,7 @@ pub fn println(string: &str) {
     print(string);
     print_char('\n');
 }
-pub fn print_num(number: u64) {
+pub fn print_num(number: usize) {
     unsafe {
         system_call(SysCall::PrintNum, number, 0);
     }
@@ -66,12 +62,8 @@ pub fn sys_yield() {
     }
 }
 pub fn uart_open() -> bool {
-    unsafe {
-        return system_call(SysCall::UartOpen, 0, 0) != 0;
-    }
+    unsafe { system_call(SysCall::UartOpen, 0, 0) != 0 }
 }
 pub fn uart_close() -> bool {
-    unsafe {
-        return system_call(SysCall::UartClose, 0, 0) != 0;
-    }
+    unsafe { system_call(SysCall::UartClose, 0, 0) != 0 }
 }
